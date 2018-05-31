069541d44d52df3f2ed4b9aac9e4169f391d888c
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2018-05-31 21:28:12 +0200

    demo test

---
+++ UiAutomationFramework/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UiAutomationFramework
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+			IWebDriver driver = new ChromeDriver();
+			//Firefox's proxy driver executable is in a folder already
+			//  on the host system's PATH environment variable.
+			driver.Navigate().GoToUrl("http://saucelabs.com");
+			IWebElement header = driver.FindElement(By.Id("site - header"));
+	
+			Assert.True(header.Displayed);
+			driver.Quit();
+		}
+	}
+}
